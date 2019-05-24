e14e09a3b08e1ece9570434287c874121fb967f5
ohashi <t_ohashi@beruf.xyz>
2019-05-24 11:59:11 +0900

    ランダムだが、必ず3件いいねするように変更

--- liketweet.py
+++ liketweet.py
@@ -22,14 +22,18 @@ auth.set_access_token(AT, AS)
 api = tweepy.API(auth)
 blocked_id = {"uiuiuieros"}
 
+count = 3 #いいねの件数
 searchWord = "((vtuber OR バーチャル) (魂 OR 中の人) -雀 (オーディション OR 募集) -放り出された exclude:retweets exclude:replies) OR @eiofkljislnf"
 search_result = api.search(q=searchWord, lang='ja', result_type='recent',tweet_mode="extended", count=100)
-for i in range(5):
+for i in range(len(search_result)):
     result = random.choice(search_result)
     if result.user.screen_name not in blocked_id:
         try:
             api.create_favorite(result.id)
             print(result.user.name + " status_id:" + str(result.id) + "をいいねしました")
+            count -= 1
+            if count == 0:
+                break
         except Exception as e:
             print(e)
 
